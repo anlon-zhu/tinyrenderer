6d46d05262e2d96e0237765332e28fb1eeac57ab
Anlon Zhu <zhu.anlon@gmail.com>
2024-05-28 19:39:14 +0100

    line update

--- main.cpp
+++ main.cpp
@@ -12,27 +12,43 @@ Model *model = NULL;
 const int width  = 800;
 const int height = 800;
 
-void line(int x0, int y0, int x1, int y1, TGAImage &image, TGAColor color) {
-    bool steep = false;
-    if (std::abs(x0-x1)<std::abs(y0-y1)) {
-        std::swap(x0, y0);
-        std::swap(x1, y1);
-        steep = true;
-    }
-    if (x0>x1) {
-        std::swap(x0, x1);
-        std::swap(y0, y1);
-    }
+void line(Vec2i p0, Vec2i p1, TGAImage &image, TGAColor color)
+{
+   bool steep = false;
+   if (std::abs(p0.x - p1.x) < std::abs(p0.y - p1.y))
+   {
+       std::swap(p0.x, p0.y);
+       std::swap(p1.x, p1.y);
+       steep = true;
+   }
+   if (p0.x > p1.x)
+   {
+       std::swap(p0, p1);
+   }
 
-    for (int x=x0; x<=x1; x++) {
-        float t = (x-x0)/(float)(x1-x0);
-        int y = y0*(1.-t) + y1*t;
-        if (steep) {
-            image.set(y, x, color);
-        } else {
-            image.set(x, y, color);
-        }
-    }
+
+   int dx = p1.x - p0.x;
+   int dy = p1.y - p0.y;
+   int derror2 = std::abs(dy) * 2;
+   int error2 = 0;
+   int y = p0.y;
+   for (int x = p0.x; x <= p1.x; x++)
+   {
+       if (steep)
+       {
+           image.set(y, x, color);
+       }
+       else
+       {
+           image.set(x, y, color);
+       }
+       error2 += derror2;
+       if (error2 > dx)
+       {
+           y += (p1.y > p0.y ? 1 : -1);
+           error2 -= dx * 2;
+       }
+   }
 }
 
 Vec3f barycentric(Vec3f A, Vec3f B, Vec3f C, Vec3f P) {
