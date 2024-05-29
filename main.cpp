ee0263cccde1be4eeaa2c3d761b62a4765ad4358
Anlon Zhu <zhu.anlon@gmail.com>
2024-05-29 14:24:40 +0100

    fun with shaders

--- main.cpp
+++ main.cpp
@@ -63,6 +63,62 @@ struct TextureShader : public IShader
     }
 };
 
+struct TextureShaderNorm : public IShader
+{
+    mat<2, 3, float> varying_uv;
+    mat<4, 4, float> uniform_M;   //  Projection*ModelView
+    mat<4, 4, float> uniform_MIT; // (Projection*ModelView).invert_transpose()
+
+    virtual Vec4f vertex(int iface, int nthvert)
+    {
+        Vec4f gl_Vertex = embed<4>(model->vert(iface, nthvert));
+        varying_uv.set_col(nthvert, model->uv(iface, nthvert));
+        return Viewport * Projection * ModelView * gl_Vertex;
+    }
+
+    virtual bool fragment(Vec3f bar, TGAColor &color)
+    {
+        Vec2f uv = varying_uv * bar;
+        // Now we get intensity from the normal mapping texture
+        // (M^-1)^T * normal * M * light = 0
+        // We can dot the normal with the light direction
+        Vec3f n = proj<3>(uniform_MIT * embed<4>(model->normal(uv))).normalize();
+        Vec3f l = proj<3>(uniform_M * embed<4>(light_dir)).normalize();
+        float intensity = std::max(0.f, n * l);
+        color = model->diffuse(uv) * intensity;
+        return false;
+    }
+};
+
+struct PhongShader : public IShader
+{
+    mat<2, 3, float> varying_uv;  // same as above
+    mat<4, 4, float> uniform_M;   //  Projection*ModelView
+    mat<4, 4, float> uniform_MIT; // (Projection*ModelView).invert_transpose()
+
+    virtual Vec4f vertex(int iface, int nthvert)
+    {
+        varying_uv.set_col(nthvert, model->uv(iface, nthvert));
+        Vec4f gl_Vertex = embed<4>(model->vert(iface, nthvert)); // read the vertex from .obj file
+        return Viewport * Projection * ModelView * gl_Vertex;    // transform it to screen coordinates
+    }
+
+    virtual bool fragment(Vec3f bar, TGAColor &color)
+    {
+        Vec2f uv = varying_uv * bar;
+        Vec3f n = proj<3>(uniform_MIT * embed<4>(model->normal(uv))).normalize();
+        Vec3f l = proj<3>(uniform_M * embed<4>(light_dir)).normalize();
+        Vec3f r = (n * (n * l * 2.f) - l).normalize(); // reflected light
+        float spec = pow(std::max(r.z, 0.0f), model->specular(uv));
+        float diff = std::max(0.f, n * l);
+        TGAColor c = model->diffuse(uv);
+        color = c;
+        for (int i = 0; i < 3; i++)
+            color[i] = std::min<float>(3 + c[i] * (0.6 * diff + .8 * spec), 255);
+        return false;
+    }
+};
+
 ////////////////////////////////////////////////////////////////////////
 int main(int argc, char **argv)
 {
@@ -83,7 +139,9 @@ int main(int argc, char **argv)
     light_dir.normalize();
     TGAImage image(width, height, TGAImage::RGB);
     TGAImage zbuffer(width, height, TGAImage::GRAYSCALE);
-    TextureShader shader;
+    PhongShader shader;
+    shader.uniform_M = Projection * ModelView;
+    shader.uniform_MIT = (Projection * ModelView).invert_transpose();
 
     // Rasterize each triangle
     for (int i = 0; i < model->nfaces(); i++)
